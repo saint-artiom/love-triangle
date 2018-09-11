412c69f713c459981c5d54713c03c16f3d41df5f
Artiom <7120681@mail.ru>
2018-09-11 14:38:54 +0300

    [fix] love triangles

--- src/index.js
+++ src/index.js
@@ -4,4 +4,21 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  if (preferences.length < 3)
+    return 0;
+
+  const l = [];
+  
+  for (let i=0; i<preferences.length; i++) {
+    let lover1 = preferences[i];
+    let lover2 = preferences[lover1-1];
+    let lover3 = preferences[lover2-1];
+    if (lover3-1 == i && lover1 != lover2 && lover2 != lover3) {
+      const triangle = [lover1,lover2,lover3].sort().toString();
+      if (l.indexOf(triangle) < 0) 
+        l.push(triangle);
+    }
+  }
+  console.log('>>', l)
+  return l.length;
 };
